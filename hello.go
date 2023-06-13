146b02bd982b764e40d332c1c0f148ae3a108cbb
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2023-06-12 22:22:56 -0300

    Primeiro commit. Começar aplicação em Golang

---
+++ hello.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	nome := "Márcio"
+	versao := 1.1
+	
+	fmt.Println("Olá, Sr. ", nome)
+	fmt.Println("Este programa está na versão ", versao)
+
+	fmt.Println("1 - Inicar Monitoramento")
+}
